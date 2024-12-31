fc9aff05b4908f5043b2e9934dfc8ef0a889d7bd
chintan166 <chintansuthar61@gmail.com>
2024-12-31 14:09:07 +0530

    loginSignup

---
+++ loginSignup/accounts/views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import User
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserRegisterSerializer
+from django.shortcuts import render
+
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'message': 'User registered successfully!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username is None or password is None:
+            return Response({'detail': 'Both username and password are required.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.filter(username=username).first()
+
+        if user is None or not user.check_password(password):
+            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        return Response({'access_token': access_token, 'refresh_token': str(refresh)}, status=status.HTTP_200_OK)
+
+def accounts(request):
+    return render(request, 'index.html')
